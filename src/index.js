4a199fbaa52467aca1bfd8221f6d226d77a67f7f
BartoszRajewicz <szejn3@gmail.com>
2023-12-09 22:03:41 +0100

    added cat-api.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_lEYZY9zF5opwIdZ4qyFEwKgAmOwcybDi0Kpo0KHFZcTcdOHXzTD9xzdg7dmmaygi';
 
-  
+const = fetchBreeds = () => {
+      
+  } 
