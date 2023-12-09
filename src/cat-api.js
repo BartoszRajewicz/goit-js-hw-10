a0394c592a75b5397819b54d1bf22bb3f5aac9c1
BartoszRajewicz <szejn3@gmail.com>
2023-12-09 22:37:44 +0100

    feat: added fetchBreeds function

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,3 +4,16 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_lEYZY9zF5opwIdZ4qyFEwKgAmOwcybDi0Kpo0KHFZcTcdOHXzTD9xzdg7dmmaygi';
 
+const apiUrl = 'https://api.thecatapi.com/v1/breeds';
+
+export const fetchBreeds = async () => {
+  try {
+    const response = await axios.get(apiUrl);
+    if (response.data.length === 0) throw new Error();
+    return response.data;
+  } catch (error) {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  }
+};
