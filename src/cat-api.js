eb707d1c3fca3f2e1d82888d9c233a78e69bf215
BartoszRajewicz <szejn3@gmail.com>
2023-12-09 22:17:50 +0100

    feat: installed notiflix and added axios to cat-api.js

---
+++ src/cat-api.js
@@ -0,0 +1,6 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_lEYZY9zF5opwIdZ4qyFEwKgAmOwcybDi0Kpo0KHFZcTcdOHXzTD9xzdg7dmmaygi';
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_lEYZY9zF5opwIdZ4qyFEwKgAmOwcybDi0Kpo0KHFZcTcdOHXzTD9xzdg7dmmaygi';
 
-const = fetchBreeds = () => {
-      
-  } 
