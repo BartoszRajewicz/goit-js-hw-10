4c0d869641b9c3aec784c9c855c2719024093c92
BartoszRajewicz <szejn3@gmail.com>
2023-12-10 21:53:46 +0100

    feat: added code for breed selection

--- src/index.js
+++ src/index.js
@@ -3,3 +3,31 @@ import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
 
+document.addEventListener('DOMContentLoaded', async () => {
+  const breedSelect = document.querySelector('.breed-select');
+  const catInfoContainer = document.querySelector('.cat-info');
+
+  try {
+    const breeds = await fetchBreeds();
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.text = breed.name;
+      breedSelect.appendChild(option);
+    });
+
+    breedSelect.addEventListener('change', async event => {
+      const selectedBreedId = event.target.value;
+
+      try {
+        const catInfo = await fetchCatByBreed(selectedBreedId);
+
+        displayCatInfo(catInfo, catInfoContainer);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
