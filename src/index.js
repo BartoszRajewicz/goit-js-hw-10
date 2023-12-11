634868b8d580cbd007fe0389559abbe9e5fbc81e
BartoszRajewicz <szejn3@gmail.com>
2023-12-11 20:08:20 +0100

    feat: added the loader solution v1

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ document.addEventListener('DOMContentLoaded', async () => {
   const loader = document.querySelector('.loader');
   const errorContainer = document.querySelector('.error');
 
+  loader.classList.add('visible');
+
   try {
-    loader.classList.add('visible'); 
     const breeds = await fetchBreeds();
     breeds.forEach(breed => {
       const option = document.createElement('option');
@@ -19,30 +20,57 @@ document.addEventListener('DOMContentLoaded', async () => {
       breedSelect.appendChild(option);
     });
 
+    loader.classList.remove('visible');
+
     breedSelect.addEventListener('change', async event => {
-      loader.classList.add('visible'); 
-      errorContainer.classList.remove('visible'); 
+      loader.classList.add('visible');
+      errorContainer.classList.remove('visible');
 
       const selectedBreedId = event.target.value;
 
       try {
         const catInfo = await fetchCatByBreed(selectedBreedId);
-
         displayCatInfo(catInfo, catInfoContainer);
       } catch (error) {
         console.error(error);
         Notiflix.Notify.failure(
           'Oops! Something went wrong! Try reloading the page!'
         );
-        errorContainer.classList.add('visible'); 
+        errorContainer.classList.add('visible');
       } finally {
-        loader.classList.remove('visible'); 
+        loader.classList.remove('visible');
       }
     });
   } catch (error) {
     console.error(error);
-    errorContainer.classList.add('visible'); // Wyświetl komunikat o błędzie
-  } finally {
-    loader.classList.remove('visible'); // Ukryj loader po zakończeniu żądania
+    errorContainer.classList.add('visible');
+    loader.classList.remove('visible');
   }
 });
+
+function displayCatInfo(catInfo, container) {
+  const { breeds, url } = catInfo;
+  const breed = breeds[0];
+
+  container.innerHTML = '';
+
+  const img = document.createElement('img');
+  img.src = url;
+  img.alt = breed.name;
+
+  const name = document.createElement('h2');
+  name.textContent = breed.name;
+
+  const desc = document.createElement('p');
+  desc.textContent = breed.description;
+
+  const temper = document.createElement('p');
+  temper.textContent = `Temperament: ${
+    breed.temperament || 'No temperament information available.'
+  }`;
+
+  container.appendChild(img);
+  container.appendChild(name);
+  container.appendChild(desc);
+  container.appendChild(temper);
+}
