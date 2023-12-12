562171d5f64e92f43d93748e2d7cf572c7772dd9
BartoszRajewicz <szejn3@gmail.com>
2023-12-12 21:49:34 +0100

    feat: added corrections to index.html, index.js and cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,3 @@
-import Notiflix from 'notiflix';
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
@@ -9,12 +8,10 @@ const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 export const fetchBreeds = async () => {
   try {
     const response = await axios.get(apiUrl);
-    if (response.data.length === 0) throw new Error();
+    if (response.data.length === 0) throw new Error('No breeds found');
     return response.data;
   } catch (error) {
-    Notiflix.Notify.failure(
-      'Oops! Something went wrong! Try reloading the page!'
-    );
+    return Promise.reject(error);
   }
 };
 
@@ -23,12 +20,10 @@ const catApiUrl = 'https://api.thecatapi.com/v1/images/search';
 export const fetchCatByBreed = async breedId => {
   try {
     const response = await axios.get(`${catApiUrl}?breed_ids=${breedId}`);
-    if (response.data.length === 0) throw new Error();
+    if (response.data.length === 0)
+      throw new Error('No cat images found for the breed');
     return response.data[0];
   } catch (error) {
-    Notiflix.Notify.failure(
-      'Oops! Something went wrong! Try reloading the page!'
-    );
     return Promise.reject(error);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,52 +1,57 @@
-import axios from 'axios';
-import { fetchBreeds } from './cat-api';
-import { fetchCatByBreed } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const breedSelect = document.querySelector('.breed-select');
-  const catInfoContainer = document.querySelector('.cat-info');
-  const loader = document.querySelector('.loader');
-  const errorContainer = document.querySelector('.error');
+const breedSelect = document.querySelector('.breed-select');
+const catInfoContainer = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const errorContainer = document.querySelector('.error');
 
-  loader.classList.add('visible');
-
-  try {
-    const breeds = await fetchBreeds();
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.text = breed.name;
-      breedSelect.appendChild(option);
-    });
-
-    loader.classList.remove('visible');
-
-    breedSelect.addEventListener('change', async event => {
+document.addEventListener('DOMContentLoaded', () => {
+  (async () => {
+    try {
       loader.classList.add('visible');
-      errorContainer.classList.remove('visible');
 
-      const selectedBreedId = event.target.value;
+      const breeds = await fetchBreeds();
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.text = breed.name;
+        breedSelect.appendChild(option);
+      });
 
-      try {
-        const catInfo = await fetchCatByBreed(selectedBreedId);
-        displayCatInfo(catInfo, catInfoContainer);
-      } catch (error) {
-        console.error(error);
-        Notiflix.Notify.failure(
-          'Oops! Something went wrong! Try reloading the page!'
-        );
-        errorContainer.classList.add('visible');
-      } finally {
-        loader.classList.remove('visible');
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    errorContainer.classList.add('visible');
-  } finally {
-    loader.classList.remove('visible');
-  }
+      breedSelect.classList.remove('hidden');
+
+      loader.classList.remove('visible');
+
+      breedSelect.addEventListener('change', async event => {
+        loader.classList.add('visible');
+        errorContainer.classList.remove('visible');
+
+        const selectedBreedId = event.target.value;
+
+        try {
+          const catInfo = await fetchCatByBreed(selectedBreedId);
+          displayCatInfo(catInfo, catInfoContainer);
+        } catch (error) {
+          console.error(error);
+
+          if (!errorContainer.classList.contains('visible')) {
+            Notiflix.Notify.failure(
+              'Oops! Something went wrong! Try reloading the page!'
+            );
+          }
+
+          errorContainer.classList.remove('visible');
+        } finally {
+          loader.classList.remove('visible');
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      errorContainer.classList.add('visible');
+      loader.classList.remove('visible');
+    }
+  })();
 });
 
 function displayCatInfo(catInfo, container) {
